8b5dd2ed1081c87f51283be79de1f7d864f179ff
Jfnrosimo <j.rosimo098.com>
2023-01-02 10:40:43 +0800

    Feat: added initial folder setup

---
+++ birthday-reminder/src/App.js
@@ -0,0 +1,12 @@
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+
+function App() {
+  return (
+    <main className="">
+      <p>APP</p>
+    </main>
+  );
+}
+
+export default App;
