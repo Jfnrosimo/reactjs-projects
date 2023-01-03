692b6b8ac9b607e7ded8c05b73ccd89fa7be00af
jrosi <j.rosimo098@gmail.com>
2023-01-03 13:42:50 +0800

    Feat: inital code setup

--- reviews/src/App.js
+++ reviews/src/App.js
@@ -1,24 +1,16 @@
 import { useState } from "react";
-
-import data from "./data";
-import List from "./List";
+import Review from "./Review";
 
 function App() {
-  const [people, setPeople] = useState(data);
-
   return (
-    <main className="bg-pink-400  text-center flex flex-col min-h-screen p-6 overflow-hidden">
-      <h1 className="text-3xl font-bold">birthday reminder</h1>
-      <section className="bg-white mx-6 shadow-md shadow-zinc-600 rounded-sm p-4">
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-        <button
-          className="bg-pink-400 w-full rounded mt-6 py-2"
-          onClick={() => setPeople([])}
-        >
-          clear all
-        </button>
+    <main>
+      <section>
+        <h2 className="text-center  mt-4 mx-auto border-b-2 border-b-blue-400 w-2/3">
+          our reviews
+        </h2>
+        <div></div>
       </section>
+      <Review />
     </main>
   );
 }
---
+++ reviews/src/Review.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import people from "./data";
+
+//Import UI
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  return <h2>review component</h2>;
+};
+
+export default Review;
