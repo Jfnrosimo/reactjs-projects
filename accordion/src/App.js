7bbbd28b2a160e3a26c25e8baf4c21c03753321c
jrosi <j.rosimo098@gmail.com>
2023-01-03 20:04:38 +0800

    Feat: finalized functionalities and added tailwind css

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -5,11 +5,11 @@ import SingleQuestion from "./Question";
 function App() {
   const [questions, setQuestions] = useState(data);
   return (
-    <main className=" bg-purple-600 min-h-screen">
-      <div>
+    <main className=" bg-purple-900 h-screen flex justify-center align-middle p-5">
+      <div className="bg-gray-300 h-auto my-auto rounded p-5">
         <h3>questions and answers about login</h3>
-        <section>
-          {data.map((question) => (
+        <section className="">
+          {questions.map((question) => (
             <SingleQuestion key={question.id} {...question} />
           ))}
         </section>
--- accordion/src/Question.js
+++ accordion/src/Question.js
@@ -2,10 +2,22 @@ import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const Question = ({ id, title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+
+  const toggleInfo = () => {
+    setShowInfo(!showInfo);
+  };
+
   return (
-    <section>
-      <h4>{title}</h4>
-    </section>
+    <article className="my-5 border border-zinc-500 p-3 max-w-xs">
+      <header className="flex justify-between">
+        <h4>{title}</h4>
+        <button onClick={toggleInfo}>
+          {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {showInfo && <p className="text-sm">{info}</p>}
+    </article>
   );
 };
 
