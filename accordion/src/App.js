d29024b50bc689333757bedf8a747f2b521e6817
jrosi <j.rosimo098@gmail.com>
2023-01-03 19:10:14 +0800

    Feat: added mapping for questions

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -3,17 +3,17 @@ import data from "./data";
 import SingleQuestion from "./Question";
 
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
     <main className=" bg-purple-600 min-h-screen">
-      <section>
-        {data.map((question) => (
-          <SingleQuestion
-            id={question.id}
-            title={question.title}
-            info={question.info}
-          />
-        ))}
-      </section>
+      <div>
+        <h3>questions and answers about login</h3>
+        <section>
+          {data.map((question) => (
+            <SingleQuestion key={question.id} {...question} />
+          ))}
+        </section>
+      </div>
     </main>
   );
 }
--- accordion/src/Question.js
+++ accordion/src/Question.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
 const Question = ({ id, title, info }) => {
   return (
     <section>
