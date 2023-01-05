58c6dadae87eee953998b60da1f981edd112aac1
jrosi <j.rosimo098@gmail.com>
2023-01-05 08:31:40 +0800

    Feat: added inital folder and file setup

---
+++ slider/src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+
+function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  return (
+    <section>
+      <div>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div>
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+          //More stuff to be added
+
+          return (
+            <article key={id}>
+              <img src={image} alt={image} />
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default App;
