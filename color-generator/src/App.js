218d50f6c030d9501861b840973d205efcf01640
jrosi <j.rosimo098@gmail.com>
2023-01-06 17:07:44 +0800

    Feat: added mapping of all color list

--- color-generator/src/App.js
+++ color-generator/src/App.js
@@ -12,6 +12,7 @@ function App() {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
+      setList(colors);
       console.log(colors);
     } catch (error) {
       setError(true);
@@ -35,7 +36,10 @@ function App() {
         <button type="submit">submit</button>
       </form>
       <section>
-        <h4>list goes here</h4>
+        {list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
       </section>
     </>
   );
