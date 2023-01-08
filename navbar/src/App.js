f4a9cae4f95d48e7de4c32f08675ff5f96be5441
Jfnrosimo <j.rosimo098.com>
2023-01-08 10:38:55 +0800

    Feat: added functionalities

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import NavbarTailwind from "./NavbarTailwind";
+
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+      <NavbarTailwind />
     </>
   );
 }
--- navbar/src/Navbar.js
+++ navbar/src/Navbar.js
@@ -3,13 +3,55 @@ import { FaBars, FaTwitter } from "react-icons/fa";
 import { links, social } from "./data";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav>
-      <div>
-        <div>
+      <div className="nav-center">
+        <div className="nav-header">
           <h1>JS</h1>
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
         </div>
-        <div></div>
+
+        <div className={`links-container`} ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((item) => {
+              const { id, url, text } = item;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <ul className="social-icons">
+          {social.map((item) => {
+            const { id, url, icon } = item;
+            return (
+              <a key={id} href={url}>
+                {icon}
+              </a>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
