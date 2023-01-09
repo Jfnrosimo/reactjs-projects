916b8244f38f3173e38a4b8366490f36f5dbc006
Jfnrosimo <j.rosimo098.com>
2023-01-09 18:48:05 +0800

    Feat: initial code setup for cart containers

--- cart/src/context.js
+++ cart/src/context.js
@@ -8,9 +8,17 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [cart, setCart] = useState(cartItems);
 
-  return <AppContext.Provider value={cart}>{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider
+      value={{
+        cart,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
-
+// make sure use
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
