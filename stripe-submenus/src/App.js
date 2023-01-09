391dd1790fcc66882e5643cbcfb2d4953710e181
Jfnrosimo <j.rosimo098.com>
2023-01-09 09:50:44 +0800

    Feat: initial folder and file setup

---
+++ stripe-submenus/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+import Modal from "./Modal";
+
+const App = () => {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  );
+};
+
+export default App;
