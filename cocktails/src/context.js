3e85a0f600e1ebe01f63901dbef2235351c24df3
Jfnrosimo <j.rosimo098.com>
2023-01-10 19:21:42 +0800

    Feat: added loading functionality

--- cocktails/src/context.js
+++ cocktails/src/context.js
@@ -5,14 +5,12 @@ const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
 
   return (
-    <AppContext.Provider
-      value={{ loading, searchTerm, cocktails, setSearchTerm }}
-    >
+    <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
       {children}
     </AppContext.Provider>
   );
