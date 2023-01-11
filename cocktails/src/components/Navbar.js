5def96000158f825ada88ed33e9edb771bd8125c
Jfnrosimo <j.rosimo098.com>
2023-01-11 16:07:07 +0800

    Feat: finishing touches on navbar and about page

--- cocktails/src/components/Navbar.js
+++ cocktails/src/components/Navbar.js
@@ -5,15 +5,15 @@ import logo from "../logo.svg";
 const Navbar = () => {
   return (
     <nav className="px-2 py-1  shadow-lg">
-      <div className="flex justify-between items-center py-3 mx-4">
+      <div className="flex justify-between items-center py-3 mx-4 lg:justify-around">
         <Link to="/">
-          <img className=" w-48" src={logo} alt="cocktail db logo" />
+          <img className=" w-56" src={logo} alt="cocktail db logo" />
         </Link>
-        <ul className="flex">
-          <li className="mx-1">
+        <ul className="flex lg:text-lg lg:font-semibold">
+          <li className="mx-1 lg:mx-4">
             <Link to="/">Home</Link>
           </li>
-          <li className="mx-1">
+          <li className="mx-1 lg:mx-4">
             <Link to="/about">About</Link>
           </li>
         </ul>
--- cocktails/src/pages/About.js
+++ cocktails/src/pages/About.js
@@ -2,14 +2,20 @@ import React from "react";
 
 const About = () => {
   return (
-    <section>
-      <h1>About</h1>
+    <section className="text-center mt-24 lg:w-3/5 lg:mx-auto">
+      <h1 className="text-3xl mb-4">About</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo ipsa
-        tempore ullam minima totam quis temporibus ratione asperiores illo,
-        deleniti vero numquam rerum eaque cum? Amet atque rem dolorem aliquam
-        alias reiciendis. Ex sit asperiores voluptatem dolorum labore porro
-        perspiciatis.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nam
+        earum assumenda consequatur quaerat laboriosam, voluptates perferendis
+        iusto. Vero earum, molestiae laborum quas unde aliquam numquam provident
+        minima? Praesentium, veritatis dolorem tenetur eligendi atque sed
+        quaerat necessitatibus quibusdam quae nemo, alias quia ex sapiente quos
+        iusto minus suscipit tempore velit itaque recusandae aperiam! Possimus
+        aliquam dolor itaque in quia ullam dolores atque fugit iure enim
+        distinctio velit dolore molestiae dignissimos corporis, magni molestias?
+        Exercitationem porro quas iure, impedit tempore placeat ratione nulla et
+        harum modi illo accusamus accusantium numquam voluptas? Id illo
+        voluptatem expedita, impedit velit iure ducimus repellat vitae.
       </p>
     </section>
   );
